9be0121a774b0a2ead2e237fd9624633e39a83d3
Lee Braddock <lee.braddock@cru.org>
2014-08-22 15:21:28 -0400

    Get staff user from Relay LDAP.

---
+++ src/main/java/org/cru/migration/domain/StaffRelayUserMap.java
@@ -0,0 +1,94 @@
+package org.cru.migration.domain;
+
+import org.ccci.idm.ldap.attributes.LdapAttributes;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class StaffRelayUserMap implements Map<String,String>
+{
+	private Map<String, String> map;
+
+	public StaffRelayUserMap(LdapAttributes ldapAttributes)
+	{
+		map = new HashMap<String, String>();
+
+		map.put(ldapAttributes.employeeNumber, "employeeId");
+		map.put(ldapAttributes.username, "username");
+		map.put(ldapAttributes.password, "password");
+		map.put(ldapAttributes.lastLogonTimeStamp, "lastLogonTimestamp");
+	}
+
+	public void clear()
+	{
+		map.clear();
+	}
+
+	public boolean containsKey(Object key)
+	{
+		return map.containsKey(key);
+	}
+
+	public boolean containsValue(Object value)
+	{
+		return map.containsValue(value);
+	}
+
+	public Set<java.util.Map.Entry<String, String>> entrySet()
+	{
+		return map.entrySet();
+	}
+
+	public boolean equals(Object o)
+	{
+		return map.equals(o);
+	}
+
+	public String get(Object key)
+	{
+		return map.get(key);
+	}
+
+	public int hashCode()
+	{
+		return map.hashCode();
+	}
+
+	public boolean isEmpty()
+	{
+		return map.isEmpty();
+	}
+
+	public Set<String> keySet()
+	{
+		return map.keySet();
+	}
+
+	public String put(String key, String value)
+	{
+		return map.put(key, value);
+	}
+
+	public void putAll(Map<? extends String, ? extends String> m)
+	{
+		map.putAll(m);
+	}
+
+	public String remove(Object key)
+	{
+		return map.remove(key);
+	}
+
+	public int size()
+	{
+		return map.size();
+	}
+
+	public Collection<String> values()
+	{
+		return map.values();
+	}
+}
+
--- src/main/java/org/cru/migration/ldap/RelayLdap.java
+++ src/main/java/org/cru/migration/ldap/RelayLdap.java
@@ -1,10 +1,17 @@
 package org.cru.migration.ldap;
 
 import com.google.common.base.Charsets;
+import com.google.common.collect.Maps;
 import com.google.common.io.Files;
 import org.ccci.idm.ldap.Ldap;
 import org.ccci.idm.ldap.attributes.LdapAttributesActiveDirectory;
+import org.ccci.idm.util.DataMngr;
+import org.ccci.idm.util.MappedProperties;
+import org.ccci.idm.util.Time;
+import org.cru.migration.domain.StaffRelayUser;
+import org.cru.migration.domain.StaffRelayUserMap;
 import org.cru.migration.support.MigrationProperties;
+import org.joda.time.DateTime;
 
 import javax.naming.NamingException;
 import javax.naming.directory.Attributes;
@@ -15,9 +22,11 @@ public class RelayLdap
 {
 	private Ldap ldap;
 
-	private LdapAttributesActiveDirectory ldapAttributes = new LdapAttributesActiveDirectory();
+	private LdapAttributesActiveDirectory ldapAttributes;
 
-	private String userDn;
+	private StaffRelayUserMap staffRelayUserMap;
+
+	private String userRootDn;
 
 	public RelayLdap(MigrationProperties properties) throws Exception
 	{
@@ -27,11 +36,46 @@ public class RelayLdap
 		ldap = new Ldap(properties.getNonNullProperty("relayLdapHost"), properties.getNonNullProperty("relayLdapUser")
 				, password);
 
-		userDn = properties.getNonNullProperty("relayUserRootDn");
+		userRootDn = properties.getNonNullProperty("relayUserRootDn");
+
+		ldapAttributes = new LdapAttributesActiveDirectory();
+
+		staffRelayUserMap = new StaffRelayUserMap(ldapAttributes);
 	}
 
-	public Map<String, Attributes> getUSStaff() throws NamingException
+	public StaffRelayUser getStaff(String employeeId) throws NamingException
 	{
-		return ldap.searchAttributes(userDn, ldapAttributes.employeeNumber + "=*", ldapAttributes.getAll());
+		StaffRelayUser staffRelayUser = new StaffRelayUser();
+		staffRelayUser.setEmployeeId(employeeId);
+
+		Map<String,String> searchMap = Maps.newHashMap();
+		searchMap.put(ldapAttributes.employeeNumber, employeeId);
+
+		String[] returnAttributes = { ldapAttributes.username, ldapAttributes.lastLogonTimeStamp };
+
+		Map<String,Attributes> results = ldap.searchAttributes(userRootDn, searchMap, returnAttributes);
+
+		MappedProperties<StaffRelayUser> mappedProperties = new MappedProperties<StaffRelayUser>(staffRelayUserMap, staffRelayUser);
+
+		for (Map.Entry<String, Attributes> entry : results.entrySet())
+		{
+			Attributes attributes = entry.getValue();
+
+			for (String attributeName : returnAttributes)
+			{
+				String attributeValue = DataMngr.getAttribute(attributes, attributeName);
+
+				if(attributeName.equals(ldapAttributes.lastLogonTimeStamp))
+				{
+					staffRelayUser.setLastLogonTimestamp(new DateTime(Time.windowsToUnixTime(Long.parseLong(attributeValue))));
+				}
+				else if(attributeName.equals(ldapAttributes.username))
+				{
+					mappedProperties.setProperty(attributeName, attributeValue);
+				}
+			}
+		}
+
+		return staffRelayUser;
 	}
 }
