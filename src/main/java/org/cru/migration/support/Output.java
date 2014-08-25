12fbeec8328ba50d96c967ab7b2ad65a8a4fd847
Lee_Braddock <lee.braddock@cru.org>
2014-08-25 17:37:20 -0400

    Remove IdentityUser print method.

--- src/main/java/org/cru/migration/support/Output.java
+++ src/main/java/org/cru/migration/support/Output.java
@@ -17,18 +17,6 @@ public class Output
 		System.out.println(string);
 	}
 
-	public static void print(IdentityUser identityUser)
-	{
-		System.out.println("ssoguid:" + identityUser.getAccount().getSsoguid());
-		System.out.println("cn:" + identityUser.getAccount().getCn());
-		System.out.println("comment:" + identityUser.getAccount().getComment());
-		System.out.println("hashed pwd:" + identityUser.getAccount().getHashedPwd());
-		System.out.println("last login:" + identityUser.getAccount().getLastLogonTimestamp());
-		System.out.println("pwd last set:" + identityUser.getAccount().getPwdLastSet());
-		System.out.println("username:" + identityUser.getAccount().getUsername());
-		System.out.println("password:" + identityUser.getAccount().getPassword());
-	}
-
 	public static void logRelayStaff(List<RelayStaff> relayStaffs, File logFile) throws IOException
 	{
 		for (RelayStaff relayStaff : relayStaffs)
