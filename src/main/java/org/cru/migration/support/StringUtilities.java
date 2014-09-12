72bcff1d9b54efcfa9da3a5902f5f16d2fa0926d
Lee Braddock <lee.braddock@cru.org>
2014-09-12 16:53:11 -0400

    Add capitalize string support methods.

--- src/main/java/org/cru/migration/support/StringUtilities.java
+++ src/main/java/org/cru/migration/support/StringUtilities.java
@@ -1,5 +1,7 @@
 package org.cru.migration.support;
 
+import org.ccci.util.strings.Strings;
+
 import java.util.Set;
 
 public class StringUtilities
@@ -15,5 +17,38 @@ public class StringUtilities
 		}
 
 		return delimited;
+	}
+
+	public static String capitalize(String line)
+	{
+		return Character.toUpperCase(line.charAt(0)) + line.substring(1);
+	}
+
+	public static String capitalize(String string, String delimiter, String regex)
+	{
+		String[] split = string.split(regex);
+
+		String result = "";
+
+		for(String value : split)
+		{
+			String capitalized = StringUtilities.capitalize(value);
+
+			result += Strings.isEmpty(result) ? capitalized : delimiter + capitalized;
+		}
+
+		return result;
+	}
+
+	public static boolean isEmail(String email)
+	{
+		return email.contains("@");
+	}
+
+	public static String capitalizeEmail(String email)
+	{
+		String[] result = email.split("@");
+
+		return capitalize(result[0], ".", "\\.") + '@' + result[1];
 	}
 }
