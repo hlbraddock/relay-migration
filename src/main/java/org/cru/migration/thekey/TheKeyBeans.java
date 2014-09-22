0bd5c87e75a586da42c0ab1b7885dc8aa7ac258e
Lee Braddock <lee.braddock@cru.org>
2014-09-22 17:32:28 -0400

    Add Base64RandomStringGenerator to spring configuration. Provide via TheKeyBeans method.

--- src/main/java/org/cru/migration/thekey/TheKeyBeans.java
+++ src/main/java/org/cru/migration/thekey/TheKeyBeans.java
@@ -1,22 +1,22 @@
 package org.cru.migration.thekey;
 
 import me.thekey.cas.service.UserManager;
-import org.cru.migration.support.Output;
+import me.thekey.cas.util.Base64RandomStringGenerator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TheKeyBeans
 {
+	static private ApplicationContext applicationContext =
+			new ClassPathXmlApplicationContext("classpath*:spring/*.xml");
+
 	public static UserManager getUserManager()
 	{
-		Output.println("Getting spring application context");
-		ApplicationContext context =
-				new ClassPathXmlApplicationContext(
-						"classpath*:spring/*.xml");
+		return (UserManager) applicationContext.getBean("service.gcxUserService");
+	}
 
-		Output.println("Getting spring bean user manager");
-		UserManager userManager = (UserManager) context.getBean("service.gcxUserService");
-
-		return userManager;
+	public static Base64RandomStringGenerator getRandomStringGenerator()
+	{
+		return (Base64RandomStringGenerator) applicationContext.getBean("base64RandomStringGenerator");
 	}
 }
