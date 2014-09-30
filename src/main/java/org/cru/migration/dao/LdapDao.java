9afeed2b8d9d451d5ec5631593a4604e8dd4ef45
Lee Braddock <lee.braddock@cru.org>
2014-09-30 09:59:49 -0400

    Add attributes to class at object class creation time.

--- src/main/java/org/cru/migration/dao/LdapDao.java
+++ src/main/java/org/cru/migration/dao/LdapDao.java
@@ -1,6 +1,8 @@
 package org.cru.migration.dao;
 
 import org.ccci.idm.ldap.Ldap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.naming.NamingException;
 import javax.naming.directory.Attribute;
@@ -8,10 +10,13 @@ import javax.naming.directory.Attributes;
 import javax.naming.directory.BasicAttribute;
 import javax.naming.directory.BasicAttributes;
 import javax.naming.directory.DirContext;
+import java.util.List;
 
 public class LdapDao
 {
 	private Ldap ldap;
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	public LdapDao(Ldap ldap)
 	{
@@ -19,7 +24,8 @@ public class LdapDao
 	}
 
 	public DirContext createStructuralObjectClass
-			(String className, String description) throws NamingException
+			(String className, String description, List<String> requiredAttributes,
+			 List<String> optionalAttributes) throws NamingException
 	{
 		DirContext schema = ldap.getContext().getSchema("");
 
@@ -31,21 +37,39 @@ public class LdapDao
 		attributes.put("DESC", description);
 		attributes.put("SUP", "top");
 		attributes.put("STRUCTURAL", "true");
-		Attribute must = new BasicAttribute("MUST", "cn");
-		must.add("objectclass");
-		attributes.put(must);
+
+		for(String requiredAttribute : requiredAttributes)
+		{
+			Attribute attribute = new BasicAttribute("MUST", requiredAttribute);
+			logger.info("adding required attribute " + requiredAttribute);
+			attribute.add("objectclass");
+			attributes.put(attribute);
+		}
+
+		for(String optionalAttribute : optionalAttributes)
+		{
+			Attribute attribute = new BasicAttribute("MAY", optionalAttribute);
+			logger.info("adding optional attribute " + optionalAttribute);
+			attribute.add("objectclass");
+			attributes.put(attribute);
+		}
 
 		// Add the new schema object for the object class
 		return schema.createSubcontext(classDefinitionContextName(className), attributes);
 	}
 
+
+	/**
+	 * WARNING: This method does not seem to work.
+	 */
 	public void addAttributeToClass(String className, String attributeName, String type) throws NamingException
 	{
 		DirContext schema = ldap.getContext().getSchema("");
 
 		Attributes attributes = new BasicAttributes(false);
 
-		attributes.put(new BasicAttribute(type, attributeName));
+		Attribute attribute = new BasicAttribute(type, attributeName);
+		attributes.put(attribute);
 
 		// Modify schema object
 		schema.modifyAttributes(classDefinitionContextName(className), DirContext.ADD_ATTRIBUTE, attributes);
@@ -53,6 +77,7 @@ public class LdapDao
 
 	public DirContext createAttribute(String attributeName, String description) throws NamingException
 	{
+		logger.info("creating attribute " + attributeName);
 		DirContext schema = ldap.getContext().getSchema("");
 
 		// Specify attributes for the schema object
