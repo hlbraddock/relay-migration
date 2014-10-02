d5c23fbe8e53bcf9cf6a278a263e80b7e3769ba4
Lee Braddock <lee.braddock@cru.org>
2014-10-02 15:04:26 -0400

    Retrieve employee ids from server file.

--- src/main/java/org/cru/migration/service/PshrService.java
+++ src/main/java/org/cru/migration/service/PshrService.java
@@ -1,13 +1,18 @@
 package org.cru.migration.service;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.Sets;
+import com.google.common.io.Files;
 import org.cru.migration.dao.DaoFactory;
 import org.cru.migration.dao.PSHRDao;
 import org.cru.migration.domain.PSHRStaff;
+import org.cru.migration.support.FileHelper;
 import org.cru.migration.support.MigrationProperties;
 
 import java.beans.PropertyVetoException;
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Set;
 
 public class PSHRService
@@ -24,6 +29,15 @@ public class PSHRService
 	public Set<PSHRStaff> getSomePshrUSStaff() throws IOException, PropertyVetoException
 	{
 		Set<PSHRStaff> pshrStaffSet = Sets.newHashSet();
+
+		File file = FileHelper.getFileToRead(migrationProperties.getNonNullProperty("employeeids"));
+
+		List<String> lines = Files.readLines(file, Charsets.UTF_8);
+
+		for(String line : lines)
+		{
+			pshrStaffSet.add(new PSHRStaff(line, "None"));
+		}
 
 		return pshrStaffSet;
 	}
