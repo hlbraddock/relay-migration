cda865797f232c53de7911299927301f88ab1056
Lee Braddock <lee.braddock@cru.org>
2014-10-27 16:27:40 -0400

    Include '$' in extended alphabet list.

--- src/main/java/org/cru/migration/dao/LdapDao.java
+++ src/main/java/org/cru/migration/dao/LdapDao.java
@@ -113,7 +113,7 @@ public class LdapDao
 		return "ClassDefinition/" + name;
 	}
 
-	private char[] extendedAlphabet = {'-','.',
+	private char[] extendedAlphabet = {'-','.', '$',
 			'a','b','c','d','e','f','g','h'
 			,'i','j','k','l','m','n','o','p','q'
 			,'r','s','t','u','v','w','x','y','z',
