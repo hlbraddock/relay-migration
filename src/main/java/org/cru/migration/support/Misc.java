ab40f1984d967745ef3d8454b53c8fbf738eb011
Lee Braddock <lee.braddock@cru.org>
2014-11-17 13:29:52 -0500

    Allow special character difference in Misc.equals(string,string)

--- src/main/java/org/cru/migration/support/Misc.java
+++ src/main/java/org/cru/migration/support/Misc.java
@@ -5,6 +5,9 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Misc
 {
 	public static <T> Integer nonNullCount(T... objects)
@@ -63,10 +66,34 @@ public class Misc
 		return "";
 	}
 
-	public static Boolean equals(String string, String string2)
+    public static Boolean equals(String string, String string2)
+    {
+       return equals(string, string2, false);
+    }
+
+    Pattern p = Pattern.compile("[\\p{Alpha}]*[\\p{Punct}][\\p{Alpha}]*");
+
+    public static Boolean equals(String string, String string2, Boolean allowSpecialCharacterDifference)
 	{
-		return (Strings.isNullOrEmpty(string) && Strings.isNullOrEmpty(string2)) ||
+        if(allowSpecialCharacterDifference)
+        {
+            string = string.replaceAll("\\s","");
+            string2 = string2.replaceAll("\\s","");
+        }
+
+        Boolean result = (Strings.isNullOrEmpty(string) && Strings.isNullOrEmpty(string2)) ||
 				(string != null && string.equals(string2));
+
+        if(!result && allowSpecialCharacterDifference)
+        {
+            Pattern p = Pattern.compile("[\\p{Alpha}]*[\\p{Punct}][\\p{Alpha}]*");
+            Matcher m = p.matcher(string);
+            Matcher m2 = p.matcher(string);
+            System.out.println("matches " + m.matches() + "," + m2.matches());
+            return m.matches() || m2.matches();
+        }
+
+        return result;
 	}
 
     public static Boolean equals(DateTime dateTime, DateTime dateTime2, Boolean allowHourDifferential)
