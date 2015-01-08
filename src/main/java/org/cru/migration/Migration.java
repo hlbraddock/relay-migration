565823e77cbb65c21942737b6b35ff438f7012ae
Lee Braddock <lee.braddock@cru.org>
2015-01-08 17:55:41 -0500

    Provision users.

--- src/main/java/org/cru/migration/Migration.java
+++ src/main/java/org/cru/migration/Migration.java
@@ -658,7 +658,7 @@ public class Migration
 
 		try
 		{
-			Action action = Action.CopyKeyUsers;
+			Action action = Action.ProvisionUsers;
 
             if (action.equals(Action.CreateCruGroups))
             {
